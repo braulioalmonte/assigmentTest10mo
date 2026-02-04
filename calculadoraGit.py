7686938cad76eb0d0be5429a64edf1028e05f082
JorgeRavelo <soyplastic2023@gmail.com>
2026-02-04 09:31:26 -0400

    added multi and division

--- calculadoraGit.py
+++ calculadoraGit.py
@@ -4,33 +4,49 @@ def suma(a,b):
 def resta(a,b):
     return a-b
 
-while True:
-    print("Menu:")
+def multiplicar(a,b):
+    return a*b
 
+def dividir(a,b):
+    if b == 0:
+        return "Error: no se puede dividir entre 0"
+    return a/b
+
+
+while True:
+    print("\nMenu:")
     print("1. Suma")
     print("2. Resta")
-    print("3. Salir")
-    
-    operation = input("")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+
+    operation = input("Elige una opción: ")
 
     match operation:
         case "1":
-            print("Suma")
-            n1 = int(input("Primer numero: "))
-            n2 = int(input("Segundo numero: "))
-            print(f"La suma de {n1 + n2} = ",suma(n1,n2))
+            n1 = int(input("Primer número: "))
+            n2 = int(input("Segundo número: "))
+            print("Resultado:", suma(n1, n2))
 
         case "2":
-            print("Resta")
-            n1 = int(input("Primer numero: "))
-            n2 = int(input("Segundo numero: "))
-            print(f"La suma de {n1 + n2} = ",resta(n1,n2))
+            n1 = int(input("Primer número: "))
+            n2 = int(input("Segundo número: "))
+            print("Resultado:", resta(n1, n2))
 
         case "3":
-            print("Adios!")
+            n1 = int(input("Primer número: "))
+            n2 = int(input("Segundo número: "))
+            print("Resultado:", multiplicar(n1, n2))
+
+        case "4":
+            n1 = int(input("Primer número: "))
+            n2 = int(input("Segundo número: "))
+            print("Resultado:", dividir(n1, n2))
+
+        case "5":
+            print("¡Adiós!")
             break
 
         case _:
-            print("No es una opcion")
-
-
+            print("No es una opción válida")
